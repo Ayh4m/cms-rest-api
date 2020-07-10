df44d6e6faad2ef7a1785813cba875a5f7263ecc
AYH4M <ayh4m@hotmail.com>
2020-07-10 22:37:17 +0300

    Added APIViewFeatures View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class APIViewFeatures(APIView):
+    """APIView Features"""
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        features = [
+            "Uses HTTP methods as function (get, post, patch, put, delete)",
+            "Is similar to traditional django view",
+            "Give you the most control over your application logic",
+            "Is mapped manually to URLs"
+        ]
+        return Response({'message': 'APIView features', 'features': features})
